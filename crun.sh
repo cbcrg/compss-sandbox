083510d66f836ae2f227345a6e6676cb5e021b0c
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2013-06-11 16:47:26 +0200

    Removed unnecessary echos

--- crun.sh
+++ crun.sh
@@ -8,9 +8,6 @@ bin_dir=`dirname "$0"`
 bin_dir=`cd "$bin_dir"; pwd`
 base_dir=$bin_dir
 
-echo $bin_dir
-echo $base_dir
-
 CLASSPATH=${base_dir}/build/classes/main/
 for jar in ${base_dir}/lib/*.jar; do
   CLASSPATH=${CLASSPATH}:$jar
